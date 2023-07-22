ac2f87684ccc08ed208b5cd25ed950f1aced6b67
Kiranpindreddy <140198208+Kiranpindreddy@users.noreply.github.com>
2023-07-22 13:56:08 +0530

    Add files via upload

---
+++ Reverse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+user_input = input("Enter a word: ")
+reversed_word = user_input[::-1]
+print("Reversed word:", reversed_word)
+
+
+# In[ ]:
+
+
+
+

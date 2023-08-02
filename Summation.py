77d2d7e10392735380c9bb7e442bc76894338bbf
Kiranpindreddy <140198208+Kiranpindreddy@users.noreply.github.com>
2023-08-02 23:51:06 +0530

    Add files via upload

---
+++ Summation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sum_list(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+sample_list = [8, 2, 3, 0, 7]
+result = sum_list(sample_list)
+print(result) 
+
+
+# In[ ]:
+
+
+
+

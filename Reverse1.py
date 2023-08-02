55ca6f14df1233fef4f4edf93cca3501332c06a8
Kiranpindreddy <140198208+Kiranpindreddy@users.noreply.github.com>
2023-08-02 23:57:18 +0530

    Add files via upload

---
+++ Reverse1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def reverse_string(input_str):
+    return input_str[::-1]
+
+sample_string = "1234abcd"
+reversed_string = reverse_string(sample_string)
+print(reversed_string)
+
+
+# In[ ]:
+
+
+
+

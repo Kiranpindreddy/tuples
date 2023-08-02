018ade50254e6caa8b367c6d831afdbdcdf60d08
Kiranpindreddy <140198208+Kiranpindreddy@users.noreply.github.com>
2023-08-03 00:07:06 +0530

    Add files via upload

---
+++ upper.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def count_upper_lower(input_str):
+    upper_count = sum(1 for char in input_str if char.isupper())
+    lower_count = sum(1 for char in input_str if char.islower())
+    
+    print("No. of Upper case characters:", upper_count)
+    print("No. of Lower case characters:", lower_count)
+
+sample_string = "The quick Brow Fox"
+count_upper_lower(sample_string)
+
+
+# In[6]:
+
+
+No.of upper case characters
+No.of lower case characters
+
+
+# In[ ]:
+
+
+
+
